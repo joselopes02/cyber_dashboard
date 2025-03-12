92867452731cf05ceedc2d18d9e4593b96ea06cd
joselopes02 <jose.cnsf@gmail.com>
2025-03-12 18:55:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'caladinho'  
-    SQLALCHEMY_DATABASE_URI = os.path.join('/home','user1', 'processor','db', 'pandora.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/user1/processor/db/pandora.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
