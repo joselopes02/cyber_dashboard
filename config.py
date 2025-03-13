345345bd1dc140c51ec4bd2afe08c1f3bf592a15
joselopes02 <jose.cnsf@gmail.com>
2025-03-13 17:04:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ class Config:
     SECRET_KEY = 'caladinho'  
     SQLALCHEMY_DATABASE_URI = 'sqlite:////home/user1/processor/db/pandora.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+        # Celery configuration
+    CELERY_BROKER_URL = 'redis://localhost:8889/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:8889/0'
