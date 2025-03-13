4997d4510af3f3fc429556fcad111e287d3d2f52
joselopes02 <jose.cnsf@gmail.com>
2025-03-13 16:53:13 +0100

    celery

---
+++ celery_app.py
@@ -0,0 +1,30 @@
+# celery_app.py
+from celery import Celery
+from app import create_app  # Assuming you have a Flask app factory in app.py
+
+def make_celery(app):
+    celery = Celery(app.import_name,
+                    broker=app.config['CELERY_BROKER_URL'],
+                    backend=app.config['CELERY_RESULT_BACKEND'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+flask_app = create_app()  # Create your Flask app instance
+celery = make_celery(flask_app)
+# In celery_app.py (or a dedicated celeryconfig.py)
+celery.conf.beat_schedule = {
+    'update-statistics-every-10-minutes': {
+        'task': 'tasks.generate_static_statistics',
+        'schedule': 600.0,  # 600 seconds = 10 minutes
+    },
+}
