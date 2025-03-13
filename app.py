add1ffb6fd6dc1272c5177d0188ba6b7340af1ee
joselopes02 <jose.cnsf@gmail.com>
2025-03-13 11:42:31 +0100

    wsgi server

--- app.py
+++ app.py
@@ -38,3 +38,6 @@ app = create_app()
 if __name__ == '__main__':
 
     app.run(host="0.0.0.0",port=8888)
+#if __name__ == '__main__':
+#    app = create_app()
+#    app.run(host="0.0.0.0",port=8888)
