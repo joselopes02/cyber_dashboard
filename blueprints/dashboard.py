4b2218bb99aadb76667147b8e932dbb18ac2e527
joselopes02 <jose.cnsf@gmail.com>
2025-03-14 14:01:21 +0100

    likn url

--- blueprints/dashboard.py
+++ blueprints/dashboard.py
@@ -38,8 +38,10 @@ def get_attacks_union():
         Attack.continent.label("continent"),
         Attack.org.label("org"),
         Attack.latitude.label("latitude"),
-        Attack.longitude.label("longitude")
+        Attack.longitude.label("longitude"),
+        db.literal('attack').label('source')  # clearly added source field
     )
+
     augmented_query = db.session.query(
         Augmented_Attacks.id.label("id"),
         Augmented_Attacks.date.label("date"),
@@ -56,10 +58,12 @@ def get_attacks_union():
         Augmented_Attacks.continent.label("continent"),
         Augmented_Attacks.org.label("org"),
         Augmented_Attacks.latitude.label("latitude"),
-        Augmented_Attacks.longitude.label("longitude")
+        Augmented_Attacks.longitude.label("longitude"),
+        db.literal('augmented_attack').label('source')  # clearly added source field
     )
-    # Use union_all to combine both queries. If you need only distinct rows, use .union() instead.
+
     return attack_query.union_all(augmented_query).subquery()
+
 
 
 
@@ -136,7 +140,7 @@ def simple_encode_filter(url: str):
     return simple_encode(url)
 
 
-@dashboard_bp.route('/record/<record_type>/<path:identifier>')
+@dashboard_bp.route('/record/<record_type>/<source>/<int:identifier>')
 @cache.cached(timeout=600)
 def record_detail(record_type, identifier):
     from urllib.parse import unquote
@@ -155,14 +159,15 @@ def record_detail(record_type, identifier):
                     attacks_union.c.protocol,
                     attacks_union.c.honeypot_name,
                     attacks_union.c.latitude,      
-                    attacks_union.c.longitude, 
+                    attacks_union.c.longitude,
+                    attacks_union.c.source,
                     URL.threat_names,
                     URL.shasum,
                     URL.malicious_flags,
                     URL.times_submitted,
                     URL.reputation
                 ).join(URL, attacks_union.c.url == URL.url, isouter=True
-                ).filter(attacks_union.c.id == identifier).first_or_404()
+                ).filter(attacks_union.c.id == identifier, attacks_union.c.source == source).first_or_404()
         download_record = Download.query.filter_by(sha256=record.shasum).first()
         return render_template('record_detail.html', record=record, record_type=record_type, download_record=download_record)
 
